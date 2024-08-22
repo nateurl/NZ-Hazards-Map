6c43f55bb01a9641e1e72ba3b4bc651913d14105
Nate <48430676+nateurl@users.noreply.github.com>
2024-08-23 10:35:28 +1200

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
           { url: "https://raw.githubusercontent.com/nateurl/natesproject/master/inlandports.geojson", name: "Inland ports" },
           { url: "https://raw.githubusercontent.com/nateurl/natesproject/master/ctsites.geojson", name: "CT sites" },
           { url: "https://raw.githubusercontent.com/nateurl/natesproject/master/fuelterminals.geojson", name: "Fuel terminals" },
-          { url: "https://raw.githubusercontent.com/nateurl/natesproject/master/hszimpactpoints.geojson", name: "HSZ  Impact points" },
+          { url: "https://raw.githubusercontent.com/nateurl/natesproject/master/hszimpactpoints.geojson", name: "HSZ Impact points" },
         ];
 
         var colors = ["#1F78B4", "#95ec6f", "#9bc8f5", "#9efefe", "#c2987c", "#ff770c", "#ffc500", "#ff0000"];
