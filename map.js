b0bd574fe3b5e18b74229271f04e0e5af804e81c
Nate <48430676+nateurl@users.noreply.github.com>
2024-08-22 17:24:58 +1200

    Create map.js

---
+++ map.js
@@ -0,0 +1,10 @@
+function initmap() {
+  require([
+        "esri/Map",
+        "esri/views/MapView",
+        "esri/layers/GeoJSONLayer",
+        "esri/renderers/UniqueValueRenderer",
+        "esri/symbols/PictureMarkerSymbol",
+        "esri/geometry/Extent"
+      ], function(Map, MapView, GeoJSONLayer, UniqueValueRenderer, PictureMarkerSymbol, Extent) {
+    document.addEventListener("DOMContentLoaded", initMap);
