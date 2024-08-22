6cdccc11c592683bc2289f104e20f4d7517cc39e
Nate <48430676+nateurl@users.noreply.github.com>
2024-08-22 17:48:29 +1200

    Update map.js

--- map.js
+++ map.js
@@ -1,12 +1,53 @@
-function initMap() {
-  require([
+document.addEventListener("DOMContentLoaded", function() {
+      console.log("DOM fully loaded and parsed");
+      require([
         "esri/Map",
         "esri/views/MapView",
         "esri/layers/GeoJSONLayer",
-        "esri/renderers/UniqueValueRenderer",
+        "esri/renderers/SimpleRenderer",
         "esri/symbols/PictureMarkerSymbol",
         "esri/geometry/Extent"
-      ], function(Map, MapView, GeoJSONLayer, UniqueValueRenderer, PictureMarkerSymbol, Extent) {
+      ], function(Map, MapView, GeoJSONLayer, SimpleRenderer, PictureMarkerSymbol, Extent) {
+        console.log("Modules loaded");
+
+        var map = new Map({
+          basemap: "streets-vector"
+        });
+        console.log("Map created");
+
+        var view = new MapView({
+          container: "viewDiv",
+          map: map,
+          extent: new Extent({
+            xmin: 165.0,
+            ymin: -48.0,
+            xmax: 179.5,
+            ymax: -33.5,
+            spatialReference: { wkid: 4326 }
+          }),
+          constraints: {
+            minZoom: 4,
+            maxZoom: 18,
+            rotationEnabled: false,
+            geometry: new Extent({
+              xmin: 165.0,
+              ymin: -48.0,
+              xmax: 179.5,
+              ymax: -33.5,
+              spatialReference: { wkid: 4326 }
+            })
+          }
+        });
+        console.log("View created");
+
+        view.when(() => {
+          console.log("Map view is ready");
+          view.ui.add("layerControls", "bottom-right");
+        }, (error) => {
+          console.error("Error loading map view:", error);
+        });
+            view.watch("scale", function(newValue) {
+      console.log("New scale:", newValue);
+    });
   });
-  }
-    document.addEventListener("DOMContentLoaded", initMap);
+});
