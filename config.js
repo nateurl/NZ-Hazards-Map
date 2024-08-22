0a8b0d3adf2d4009ad334dbcb51b93e0687afcdb
Nate <48430676+nateurl@users.noreply.github.com>
2024-08-23 11:24:18 +1200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
         var layerInfo = [
           { url: "https://raw.githubusercontent.com/nateurl/natesproject/master/rail.geojson", name: "Rail" },
+          { url: "https://raw.githubusercontent.com/nateurl/natesproject/master/rail.geojson", name: "State highways" },
           { url: "https://raw.githubusercontent.com/nateurl/natesproject/master/WoolworthsDCs.geojson", name: "Woolworths DCs" },
           { url: "https://raw.githubusercontent.com/nateurl/natesproject/master/seaports.geojson", name: "Seaports" },
           { url: "https://raw.githubusercontent.com/nateurl/natesproject/master/airports.geojson", name: "Airports" },
@@ -9,4 +10,4 @@
           { url: "https://raw.githubusercontent.com/nateurl/natesproject/master/hszimpactpoints.geojson", name: "HSZ Impact points" },
         ];
 
-        var colors = ["#1F78B4", "#95ec6f", "#9bc8f5", "#9efefe", "#c2987c", "#ff770c", "#ffc500", "#ff0000"];
+        var colors = ["#1F78B4", "#ff0000", "#95ec6f", "#9bc8f5", "#9efefe", "#c2987c", "#ff770c", "#ffc500", "#ff0000"];
