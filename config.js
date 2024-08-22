0c06dde6dc3d1359594430827e02c6b442bc52a8
Nate <48430676+nateurl@users.noreply.github.com>
2024-08-23 11:33:27 +1200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
         var layerInfo = [
           { url: "https://raw.githubusercontent.com/nateurl/natesproject/master/rail.geojson", name: "Rail" },
-          { url: "https://raw.githubusercontent.com/nateurl/natesproject/master/rail.geojson", name: "State highways" },
+          { url: "https://raw.githubusercontent.com/nateurl/natesproject/master/statehighways.geojson", name: "State highways" },
           { url: "https://raw.githubusercontent.com/nateurl/natesproject/master/WoolworthsDCs.geojson", name: "Woolworths DCs" },
           { url: "https://raw.githubusercontent.com/nateurl/natesproject/master/seaports.geojson", name: "Seaports" },
           { url: "https://raw.githubusercontent.com/nateurl/natesproject/master/airports.geojson", name: "Airports" },
